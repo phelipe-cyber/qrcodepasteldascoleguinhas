5076b68180ca2ea17d00c8e760161b8d6d90ea84
phelipe-cyber <61544699+phelipe-cyber@users.noreply.github.com>
2022-03-04 14:14:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-include 'vendor/autoload.php';
-use BaconQrCode\Renderer\ImageRenderer;
-use BaconQrCode\Renderer\Image\SvgImageBackEnd;
-use BaconQrCode\Renderer\RendererStyle\RendererStyle;
-use BaconQrCode\Writer;
-$renderer = new ImageRenderer(
-    new RendererStyle(400),
-    new SvgImageBackEnd()
-);
-$writer = new Writer($renderer);
-//Exibe o QR code na tela
-$data = 'http://cardapiopasteldascoleguinhas.sistemaph.com.br/';
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QRCode Pastel das coleguinhas</title>
+</head>
 
-echo ($writer->writeString($data));
+<body>
+    <?php
+
+    include 'vendor/autoload.php';
+
+    use BaconQrCode\Renderer\ImageRenderer;
+    use BaconQrCode\Renderer\Image\SvgImageBackEnd;
+    use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+    use BaconQrCode\Writer;
+
+    $renderer = new ImageRenderer(
+        new RendererStyle(400),
+        new SvgImageBackEnd()
+    );
+    $writer = new Writer($renderer);
+    //Exibe o QR code na tela
+    $data = 'http://cardapiopasteldascoleguinhas.sistemaph.com.br/';
+
+    echo ($writer->writeString($data));
+
+    ?>
+</body>
+
+</html>
