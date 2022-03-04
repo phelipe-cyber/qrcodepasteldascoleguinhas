ef99e36d04928ad152ac42a24d3e0a65124e5846
phelipe-cyber <61544699+phelipe-cyber@users.noreply.github.com>
2022-03-04 14:24:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,26 +9,49 @@
 </head>
 
 <body>
-    <?php
 
-    include 'vendor/autoload.php';
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-    use BaconQrCode\Renderer\ImageRenderer;
-    use BaconQrCode\Renderer\Image\SvgImageBackEnd;
-    use BaconQrCode\Renderer\RendererStyle\RendererStyle;
-    use BaconQrCode\Writer;
+    <div class="card">
+        <div class="card-body text-center">
+            <h1>
+                CARDÁPIO
+            </h1>
+        </div>
+    </div>
+    <br>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
 
-    $renderer = new ImageRenderer(
-        new RendererStyle(300),
-        new SvgImageBackEnd()
-    );
-    $writer = new Writer($renderer);
-    //Exibe o QR code na tela
-    $data = 'http://cardapiopasteldascoleguinhas.sistemaph.com.br/';
 
-    echo ($writer->writeString($data));
+        <div class="col">
+            <div class="card h-100" style="width: 20rem;">
+                <div class="card-body">
+                    <?php
+                    include 'vendor/autoload.php';
 
-    ?>
+                    use BaconQrCode\Renderer\ImageRenderer;
+                    use BaconQrCode\Renderer\Image\SvgImageBackEnd;
+                    use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+                    use BaconQrCode\Writer;
+
+                    $renderer = new ImageRenderer(
+                        new RendererStyle(400),
+                        new SvgImageBackEnd()
+                    );
+                    $writer = new Writer($renderer);
+                    //Exibe o QR code na tela
+                    $data = 'http://cardapiopasteldascoleguinhas.sistemaph.com.br/';
+
+                    echo ($writer->writeString($data));
+
+                    ?>
+
+                </div>
+            </div>
+        </div>
+
 </body>
 
 </html>
