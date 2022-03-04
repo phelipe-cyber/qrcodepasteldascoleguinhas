d4be6669e4fb3764a6397f09b75280b4de6069a6
phelipe-cyber <61544699+phelipe-cyber@users.noreply.github.com>
2022-03-04 14:25:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,15 +13,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <div class="card">
-        <div class="card-body text-center">
-            <h1>
-                CARDÁPIO
-            </h1>
-        </div>
-    </div>
-    <br>
     <div class="row row-cols-1 row-cols-md-3 g-4">
 
 
